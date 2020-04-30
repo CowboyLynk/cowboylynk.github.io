ba1cf98e8e001c20c1ccf1f17edfc19c10cacded
Cowboy Lynk <clynk@mit.edu>
2020-04-30 15:19:57 -0400

    got blur and animatino working

--- js/main.js
+++ js/main.js
@@ -1,40 +1,27 @@
-function presentProject(projectID) {
-    let myDivElement = $("#blur");
-    myDivElement.hidden = true;
-    console.log("here fucker")
+function updateModalProject(projectID) {
+    $(function(){
+        let blur =  $("#blur");
+        blur.show();
+        blur.scrollTop(0);
+        blur.toggleClass("loading", false);
+
+        let project = $("#project");
+        project.load("projects/" + projectID + ".html");
+        $("#project_container").toggleClass("loading", false);
+    });
 }
 
-presentProject("planit");
+$(function() {
+    $("div.card").on("click", function(e){
+        updateModalProject(this.id);
 
+        e.preventDefault(); //to prevent any other unwanted behavior clicking the div might cause
+    });
 
-
-// Initial setup of click events
-let body = document.body,
-    blurOverlay = document.querySelector('.overlay'),
-    overlayToggles = document.querySelectorAll('div[class$="overlay"]'),
-    projectContent = document.querySelector('#modal_project_content');
-
-
-[].forEach.call(overlayToggles, function(div) {
-    div.addEventListener('click', function() {
-        /* Detect the div class name */
-        let overlayOpen = this.classList.contains('open-overlay');
-
-        /* Toggle the aria-hidden state on the overlay and the
-        no-scroll class on the body */
-        blurOverlay.setAttribute('aria-hidden', !overlayOpen);
-        body.classList.toggle('noscroll', overlayOpen);
-
-        projectContent.setAttribute('src', 'projects/' + this.id + '.html');
-        projectContent.onload = function() {
-            console.log("here2");
-            console.log(this.contentWindow.document.body.scrollHeight);
-            this.style.height = this.contentWindow.document.body.scrollHeight + 'px';
-        }
-
-        /* On some mobile browser when the overlay was previously
-        opened and scrolled, if you open it again it doesn't
-        reset its scrollTop property */
-        blurOverlay.scrollTop = 0;
-    }, false);
+    $("#blur").on("click", function(e){
+        $(this).hide();
+        $(this).toggleClass("loading", true);
+        $("#project_container").toggleClass("loading", true);
+        // e.preventDefault(); //to prevent any other unwanted behavior clicking the div might cause
+    })
 });
