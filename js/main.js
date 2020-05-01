3e401ebf053bdda10d71bcd0e395d5c158b7a39e
Cowboy Lynk <clynk@mit.edu>
2020-05-01 19:49:37 -0400

    got dynamic grid items working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
+let projects = [
+    {
+        "name": "Gazer",
+        "pid": "gazer",
+        "tags": ["Swift", "Open CV", "ARKit"]
+    },
+    {
+        "name": "Planit",
+        "pid": "planit",
+        "tags": ["Django", "Python", "HTML", "JS", "CSS"]
+    },
+    {
+        "name": "Scribr",
+        "pid": "scribr",
+        "tags": ["Python", "Flask", "FFmpeg"]
+    }
+];
+
+function initProjects() {
+    let tbody = document.querySelector('#project-grid');
+    let template = document.querySelector('#project-item-template');
+    console.log(projects);
+    console.log(projects.length);
+    for (let i = 0; i < projects.length; i++) {
+        console.log("here");
+        let project = projects[i];
+        let clone = template.content.cloneNode(true);
+
+        let projectCard = clone.querySelector("#project-card");
+        projectCard.id = project["pid"];
+
+        let projectTitle = clone.querySelector("#project-title");
+        projectTitle.textContent = project["name"];
+
+        let tagsContainer = clone.querySelector(".tags");
+        let tags = project["tags"];
+        for (let j = 0; j < tags.length; j++) {
+            let tag = document.createElement("span");
+            tag.className = "tag";
+            tag.textContent = tags[j];
+            tagsContainer.appendChild(tag);
+        }
+
+        tbody.appendChild(clone);
+    }
+}
+
+
 function showModalProject(projectID) {
     let blur =  $("#blur");
     blur.show();
@@ -27,6 +75,9 @@ function hideModalProject() {
 }
 
 $(function() {
+    // Init projects
+    initProjects();
+
     // Set up click events
     $("div.card").on("click", function(e){
         showModalProject(this.id);
@@ -39,7 +90,7 @@ $(function() {
             hideModalProject();
             e.preventDefault(); //to prevent any other unwanted behavior clicking the div might cause
         }
-    })
+    });
 
     // Check id of page
     let searchParams = new URLSearchParams(window.location.search);
